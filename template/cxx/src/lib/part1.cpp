ebf9f9fbb1a1fccba31caef0ae4515862f1f5634
Sven Gambel <sven.gambel@ul.com>
2022-12-04 10:25:15 +0100

    Update cxx template

--- template/cxx/src/lib/part1.cpp
+++ template/cxx/src/lib/part1.cpp
@@ -3,7 +3,7 @@
 Part1::Part1()
 {}
 
-auto Part1::handle_input(std::istream& is) -> std::size_t
+auto Part1::handle_input(std::istream& in) -> std::size_t
 {
   return 0;
 }
--- template/cxx/src/lib/part2.cpp
+++ template/cxx/src/lib/part2.cpp
@@ -3,7 +3,7 @@
 Part2::Part2()
 {}
 
-auto Part2::handle_input(std::istream& is) -> size_t
+auto Part2::handle_input(std::istream& in) -> size_t
 {
   return 0;
 }
